30c4401a42ee706e873923bb44fc6d46c10be449
AlpeshPrajapati02 <prajapati.alpesh.2024@gmail.com>
2024-04-09 19:28:08 +0530

    update database schema, auth controller done and multer configuration done

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,12 @@
-const express = require('express');
-const { allUser } = require('../controllers/userController');
+const express = require("express");
+const { allUser } = require("../controllers/userController");
 const router = express.Router();
-
+const { imgStorage, fileStorage } = require("../utils/multer");
+const multer = require("multer");
+const imgUpload = multer({ storage: imgStorage });
+const fileUpload = multer({ storage: fileStorage });
 
 router.route("/user")
-.get(allUser)
-
+.get(allUser);
 
 module.exports = router;
