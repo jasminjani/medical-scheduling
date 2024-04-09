f5d11fb2f868d627b69042319b3d578788907c76
DixitKaneriya8502 <dixit.kaneriya.2024@gmail.com>
2024-04-09 19:20:52 +0530

    review controller done

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const { allUser } = require('../controllers/userController');
+const { rating } = require('../controllers/ratingController');
 const router = express.Router();
 
 
-router.route("/user")
-.get(allUser)
+router.route("/user").get(allUser)
+
+router.route("/:patient_id/review/:doctor_id").post(rating)
 
 
 module.exports = router;
