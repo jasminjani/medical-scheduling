fa372580caeca8b64bfc7e39fdc50a89e9b8b856
Darshan191002 <darshan.panchal2024@gmail.com>
2024-04-10 19:13:33 +0530

    Slots all controller almost done

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 
-const { createSlots, getSlots } = require('../controllers/slotController');
+const { createSlots, getSingleSlots, bookingSlot, getAllSlots } = require('../controllers/slotController');
 
 const { rating } = require('../controllers/ratingController');
 const router = express.Router();
@@ -11,12 +11,11 @@ const imgUpload = multer({ storage: imgStorage });
 const fileUpload = multer({ storage: fileStorage });
 const { allUser, createUser } = require("../controllers/userController");
 
-
-
 router.route("/:patient_id/review/:doctor_id").post(rating);
 
 router.route("/slot/:doctor_id").post(createSlots)
-router.route("/slot/:doctor_id/:date").get(getSlots);
-router.route("/")
+router.route("/slot/:doctor_id/:date").get(getSingleSlots);
+router.route("/:patient_id/book/:slot_id").post(bookingSlot);
+router.route("/slots/:doctor_id").get(getAllSlots);
 
 module.exports = router;
