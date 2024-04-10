d30c5b99b8b2ce0d497aaa17ad88a2b9518549f1
DixitKaneriya8502 <dixit.kaneriya.2024@gmail.com>
2024-04-10 11:52:22 +0530

    added to review controller

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,5 @@
 
 const express = require('express');
-const { allUser } = require('../controllers/userController');
 const { rating } = require('../controllers/ratingController');
 const router = express.Router();
 const { createSlots } = require('../controllers/slotController');
@@ -10,8 +9,8 @@ const imgUpload = multer({ storage: imgStorage });
 const fileUpload = multer({ storage: fileStorage });
 
 
+// router.route("/user").get(allUser);
 router.route("/:patient_id/review/:doctor_id").post(rating)
-router.route("/user").get(allUser);
 router.route("/slot/:doctor_id").post(createSlots)
 
 
