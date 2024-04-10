1b84e928f4cd32005dea2d07f26c6da09ed5fa53
Darshan191002 <darshan.panchal2024@gmail.com>
2024-04-10 10:27:30 +0530

    root router done

--- routes/paymentsRouter.js
+++ routes/paymentsRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { paymentsController } = require('../controllers/paymentsController');
-const router1 = express.Router();
+const router = express.Router();
 
-router1.route('/payments').post(paymentsController);
+router.route('/payments').post(paymentsController);
 
-module.exports = router1;
+module.exports = router;
