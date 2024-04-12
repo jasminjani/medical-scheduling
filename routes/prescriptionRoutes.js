e87167e0d0bf73dbf345a68d15695ea65727eaa2
Grishmasao15 <grishma.sao.2024@gmail.com>
2024-04-12 19:33:37 +0530

    added update functionality in prescription and made template for prescription generation and updation

--- routes/prescriptionRoutes.js
+++ routes/prescriptionRoutes.js
@@ -5,9 +5,15 @@ const {
   getPrescriptionOfUser,
   getPrescriptionOfDoctor,
   generatePDF,
+  updatePrescription,
+  updateDetails,
 } = require("../controllers/prescriptionController");
 
 router.route("/createprescription/").post(createPrescription);
+
+// router.route("/updatedetails/:patient_id").get(updateDetails);
+
+// router.route("/updatePrescription").post(updatePrescription);
 
 router.route("/getprescriptionofuser/:patient_id").get(getPrescriptionOfUser);
 
