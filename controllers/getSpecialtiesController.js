90ebf094bd5598797b02781e78a89315d8184df3
Ashish Zapadiya <ashish.zapadiya.2024@gmail.com>
2024-04-15 15:00:18 +0530

    A2 : Doctors Specialties page not both side

--- controllers/getSpecialtiesController.js
+++ controllers/getSpecialtiesController.js
@@ -3,28 +3,42 @@ const conn = require("../config/dbConnection")
 exports.getSpecialties = async (req, res) => {
 
   try {
-    const [result] = await conn.query('select id,speciality from specialities');
+    const [result] = await conn.query('select id,speciality from specialities where status = 1');
     res.json(result);
   } catch (error) {
-    return res.send("DB error")
+    console.log(error);
   }
 }
 
 exports.deleteSpecialty = async (req, res) => {
   // console.log(req.body.id);
   try {
-    await conn.query('delete from specialities where id =?', [req.body.id]);
+    await conn.query('update specialities set status = 0 where id = ?', [req.body.id]);
+    res.status(200).send();
   } catch (error) {
     console.log(error);
   }
 }
 
-exports.addSpecialty = async (req, res) => {
+exports.getNewSpecialties = async (req, res) => {
 
-  console.log(req.body);
   try {
+    const [result] = await conn.query('select id,speciality from specialities where status = 0');
+    res.json(result);
 
   } catch (error) {
-
+    console.log(error);
   }
+}
+
+exports.addNewSpecialties = async (req, res) => {
+
+  try {
+    await conn.query('update specialities set status = 1 where id = ?', [req.body.id]);
+    res.status(200).send();
+
+  } catch (error) {
+    console.log(error);
+  }
+
 }
--- routes/adminPanelRouter.js
+++ routes/adminPanelRouter.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const { adminPanelControllers } = require('../controllers/adminPanelController')
-const { getSpecialties, deleteSpecialty } = require('../controllers/getSpecialtiesController')
+const { getSpecialties, deleteSpecialty, getNewSpecialties, addNewSpecialties } = require('../controllers/getSpecialtiesController')
 const adminRouter = express.Router()
 
 adminRouter.route('/admin').get(adminPanelControllers);
 adminRouter.route('/get-specialties').get(getSpecialties);
 adminRouter.route('/delete-specialty').post(deleteSpecialty);
+adminRouter.route('/get-new-specialties').get(getNewSpecialties);
+adminRouter.route('/add-specialty').post(addNewSpecialties);
 
 
-module.exports = {adminRouter}
+module.exports = { adminRouter }
