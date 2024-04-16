8b86dffe5468a5a4a1f1f54e29481be53e5831c7
yash.babaria <yash.babaria@esparkbizmail.com>
2024-04-16 18:34:28 +0530

    To Add passport  middleware doctor module router view patient history and payment history UI and fetch data backend integration done & update Database

--- routes/doctorSpecialitiesRouter.js
+++ routes/doctorSpecialitiesRouter.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const {createSpecialities,allSpecialities} = require('../controllers/doctorSpecialitiesController')
+const { createSpecialities, allSpecialities } = require('../controllers/doctorSpecialitiesController')
 
 
 router.route('/specialities')
-.post(createSpecialities)
+  .post(createSpecialities)
 
 router.route('/specialities')
-.get(allSpecialities)
+  .get(allSpecialities)
 
 
 
