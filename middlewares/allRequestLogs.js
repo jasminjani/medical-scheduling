f77e55af01b23996afd58a33c2da3ee73394710e
Ashish Zapadiya <ashish.zapadiya.2024@gmail.com>
2024-04-19 19:25:49 +0530

    A2 : change on request middleware

--- middlewares/allRequestLogs.js
+++ middlewares/allRequestLogs.js
@@ -2,16 +2,21 @@ const fs = require('fs');
 
 exports.allRequestLogs = (req, res, next) => {
   try {
-    let fullURL = `${req.ip.split(':').pop()} ${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl} \n`;
+
+    let time = new Date().toLocaleString();
+
+    // let fullURL = `${req.ip.split(':').pop()} ${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl} \n`;
+    let fullURL = `${time} ${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl} \n`;
 
     let fileName = 'uploads/requestLogs/requestURL.log';
+
     fs.appendFileSync(fileName, fullURL, (err) => {
       if (err) {
         console.log("Error in write URL");
       }
     });
   } catch (error) {
-    console.log("some error in request.log middleware");
+    console.log("some error in allRequestLogs.js middleware so create folder  : uploads/requestLogs");
   }
 
   next()
