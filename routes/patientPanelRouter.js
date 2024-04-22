86f52369c39c72e3bd53d2bfa4bfed8c4844f949
Ashish Zapadiya <ashish.zapadiya.2024@gmail.com>
2024-04-22 15:46:53 +0530

    A2 : add dynamic status in patient panel

--- routes/patientPanelRouter.js
+++ routes/patientPanelRouter.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const passport = require('passport')
-const { patientDashboard } = require('../controllers/patientModule/patientDashboardControllers');
+const { patientDashboard, patientStatus } = require('../controllers/patientModule/patientDashboardControllers');
 
 const patientRouter = express.Router();
 patientRouter.use(passport.authenticate('jwt', { session: false, failureRedirect: '/login' }))
 
 
-patientRouter.route('/patient').get(passport.authenticate('jwt',{session:false,failureRedirect:"/login"}),patientDashboard)
+patientRouter.route('/patient').get(passport.authenticate('jwt', { session: false, failureRedirect: "/login" }), patientDashboard)
+patientRouter.route('/patient-status/:id').get(passport.authenticate('jwt', { session: false, failureRedirect: "/login" }), patientStatus)
 
 
 
