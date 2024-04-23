723752acb65cea7201c0976313a14027e1ef49af
Darshan191002 <darshan.panchal2024@gmail.com>
2024-04-23 14:57:26 +0530

    Validation done for selecting date in between one week and some changes for making slot which is visited by patient go in past slots

--- public/javascript/slotPanel/slotPanel.js
+++ public/javascript/slotPanel/slotPanel.js
@@ -1,30 +1,32 @@
-let form = document.getElementById("myform");
-let rows = form.getElementsByClassName("row");
-let today = new Date();
-let minDate = today.toISOString().substring(0, 10);
+const form = document.getElementById("myform");
+const rows = form.getElementsByClassName("row");
+const today = new Date();
+const currentDay = today.getDay();
+const minDate = today.toISOString().substring(0, 10);
 
 window.location.href.split("/").pop() === "addSlot" ? document.getElementById("A3-add").style.backgroundColor = "#3984af" : "";
 
-// const inputDate = "2022-06-29 15:16:21";
-// const newInputDate = inputDate.replace(" ", "T");
-
-// const timeStamp = new Date(newInputDate).getTime();
-// console.log(new Date(timeStamp));
+// Date validation
+const sundayOffset = 7 - currentDay;
+const sunday = new Date(today);
+sunday.setDate(today.getDate() + sundayOffset);
 
 for (let i = 0; i < rows.length; i++) {
   const addButton = rows[i].getElementsByClassName("A3-buttons");
   const times = rows[i].getElementsByClassName("times")[0];
-  const error = rows[i].getElementsByClassName("error")[0];
   const date = rows[i].getElementsByClassName("date")[0];
   date.getElementsByTagName("input")[0].setAttribute("min", minDate);
   addButton[0].addEventListener("click", (e) => {
     const d = date.getElementsByTagName("input")[0].value;
     if (!d) return Swal.fire("Please select a date!!");
+    if(new Date(d) > sunday) return Swal.fire("Please select date of this week only");
     if (times.children.length > 1 && !times.children[times.children.length - 2].value) return Swal.fire("Please fill previous slot!!");
     const newNode = document.createElement("input");
     newNode.setAttribute("type", "text");
     newNode.setAttribute("name", `day${i + 1}`);
-    newNode.setAttribute("class", "A3-time")
+    newNode.setAttribute("class", "A3-time");
+    newNode.style.position = "relative";
+    newNode.addEventListener("mouseover",handleFocus(newNode));
     times.insertBefore(newNode, times.children[times.children.length - 1])
       .addEventListener("change", (e) => {
         let startTime = e.target.value;
@@ -61,4 +63,10 @@ const handleGenerate = async () => {
     });
   }
 
+}
+
+const handleFocus = (input) => {
+  const popUp = document.createElement("input");
+  popUp.setAttribute("type","time");
+  popUp.setAttribute("id","popup");
 }
